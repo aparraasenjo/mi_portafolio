480ed096d3c3688d88796fb643ddb1a8293325d8
Alberto Parra <a.parra.asenjo@gmail.com>
2020-11-11 21:57:14 -0300

    versión final con toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,10 @@ $(function () {
             hide: 800,
             }
         });    
+    $('.card-title').click(function() {
+        $('.card-text').toggle( "slow", function() {
+        
+        });
+    });
 })
+
